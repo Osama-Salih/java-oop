3953a1fe45f6e64ede682fa6c2586d7e6ca28eb0
Osama Salih <osamasalih.ca@gmail.com>
2025-01-22 21:18:27 +0300

    ArrayList, Declaration, Print, Methods(Add, Get, AddAll, Copy

--- src/App.java
+++ src/App.java
@@ -1,9 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-       final Student1 s1 = new Student1(123, "ali", 2000);
+    public static void main(String[] args) {
+    //    ArrayList<Integer> list = new ArrayList<>(); // default 10 capacity
+    //    ArrayList<Integer> list = new ArrayList<>(100); // initialCapacity
+    
+    //    ArrayList<Integer> list = new ArrayList<>(Arrays.asList(10, 20, 30, 40)); // Copy constructor.
+    //    ArrayList<Integer> list2 = new ArrayList<>(list);
+    //    ArrayList<Integer> list3;
+    //    list3 = (ArrayList) list.clone();
+        // list.add(2);
+        // System.out.println(list2);
 
-        s1 = new Student1(123, "ali", 2000);
+    //    ArrayList<Integer> list = new ArrayList<>();
+    //    list.add(10);  
+    //    list.add(20);  
+    //    list.add(30);  
 
+    //    ArrayList<Integer> list2 = new ArrayList<>();
+    //    list2.add(55);
+    //    list2.add(10);
+    //    list2.add(20);
+
+    //    list2.addAll(list);
+    // list2.addAll(Arrays.asList(56, 57, 58));
+    // list2.addAll(1, Arrays.asList(30, 40, 50));
+    //    System.out.println(list2);
+
+
+       // Add methods
+    //    list.add(list.size(),40);
+    //    list.add(0,40);
+    //    list.add(40);
+    //    System.out.println(list);
+
+
+
+    //    int x = list.get(2);m
+    //    System.out.println(list);
+       
+    //    int size = list.size();
+    //     for (int i = 0; i < size; i++) {
+    //         System.out.println(list.get(i));
+    //     }
+
+    //    for (Integer el : list) {
+    //     System.out.println(el);
+    //    }
     
     }
 }
+ 
