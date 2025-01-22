5be99c58b2d1c0703c4db2f49e84deeeab7d73dd
Osama Salih <osamasalih.ca@gmail.com>
2025-01-22 22:31:25 +0300

    ArrayList Methods(Set, Remove, Remove All, Remove Range)

--- src/App.java
+++ src/App.java
@@ -1,53 +1,42 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class App {
     public static void main(String[] args) {
-    //    ArrayList<Integer> list = new ArrayList<>(); // default 10 capacity
-    //    ArrayList<Integer> list = new ArrayList<>(100); // initialCapacity
+    //    ArrayList<Integer> list = new ArrayList<>(Arrays.asList(10, 20, 30));
+    //    ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        // list.removeIf(num -> num % 2 == 0);
+        // System.out.println(list);
+        
+        // ArrayList list = new ArrayList<>(Arrays.asList("AA", 'c',23, true));
+        // System.out.println(list);
+        // Object arr[] = new Object[5];
+        // arr[0] = "AA";
+        // arr[1] = 'C';
+        // arr[2] = true;
+        // arr[3] = 10;
+        // System.out.println(arr);
+
+        // for (Object el: arr) {
+        //     System.out.println(el);
+        // }
+
+
+
+    //    ArrayList<String> list = new ArrayList<>(Arrays.asList("A", "B", "C","A", "A"));
+        // list.set(0, 66);
+        // list.remove(0); remove by index
+        // list.remove(new Integer(10)); // remove by object
+        // list.remove((Integer) 10); // remove by object
+        // list.remove(Integer.valueOf(10)); // remove by object
+        // list.remove("10"); // remove by object string
+
+        // list.removeAll(Collections.singleton("A"));
+        // list 
+
     
-    //    ArrayList<Integer> list = new ArrayList<>(Arrays.asList(10, 20, 30, 40)); // Copy constructor.
-    //    ArrayList<Integer> list2 = new ArrayList<>(list);
-    //    ArrayList<Integer> list3;
-    //    list3 = (ArrayList) list.clone();
-        // list.add(2);
-        // System.out.println(list2);
-
-    //    ArrayList<Integer> list = new ArrayList<>();
-    //    list.add(10);  
-    //    list.add(20);  
-    //    list.add(30);  
-
-    //    ArrayList<Integer> list2 = new ArrayList<>();
-    //    list2.add(55);
-    //    list2.add(10);
-    //    list2.add(20);
-
-    //    list2.addAll(list);
-    // list2.addAll(Arrays.asList(56, 57, 58));
-    // list2.addAll(1, Arrays.asList(30, 40, 50));
-    //    System.out.println(list2);
-
-
-       // Add methods
-    //    list.add(list.size(),40);
-    //    list.add(0,40);
-    //    list.add(40);
-    //    System.out.println(list);
-
-
-
-    //    int x = list.get(2);m
-    //    System.out.println(list);
-       
-    //    int size = list.size();
-    //     for (int i = 0; i < size; i++) {
-    //         System.out.println(list.get(i));
-    //     }
-
-    //    for (Integer el : list) {
-    //     System.out.println(el);
-    //    }
     
     }
 }
