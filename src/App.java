7973eb7ffc3f69972032c8f87536daaf2fe70444
Osama Salih <osamasalih.ca@gmail.com>
2025-01-26 22:13:16 +0300

    Anonymous Inner Class (Extends Class, Implements Interface, Defined Arguments, Lambda)

--- src/App.java
+++ src/App.java
@@ -1,4 +1,23 @@
 public class App {
-    public static void main(String[] args) {
+    public interface Showable {
+        public void print();
     }
+
+    static void display(Showable s) {
+        s.print();
+    }
+    public static void main(String[] args) {
+        display(new Showable() {
+            @Override            
+            public void print() {
+                System.out.println("Showable");
+            }
+        });
+
+    //   Showable showable;
+
+    //   showable = () -> System.out.println("showable..");
+    //   showable.print();
+      };
+    
 }  
