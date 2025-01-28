1c520db02cfc0596fe866f194dcbd60ed03301b6
Osama Salih <osamasalih.ca@gmail.com>
2025-01-28 10:01:07 +0300

    Enum with switch, Attributes & Methods

--- src/App.java
+++ src/App.java
@@ -1,51 +1,43 @@
-// import java.util.ArrayList;
+import java.time.DayOfWeek;
+import java.time.Month;
 
 public class App {
-    static void m() {
-        new TestClass(5){
-            @Override
-            public void testMethod() {
-                System.out.println(k);
-            }
-
-            public void testMethod2() {
-                System.out.println();
-            } 
-        };
-    }
-    // var x = 4; // is not allowed here
-    // static void print(var x) {} is not allowed here
-    // static var print() {} is not allowed here
     public static void main(String[] args) {
-        int d = 9;
-        // ArrayList<Integer> list = new ArrayList<>();
-        // ArrayList<var> list = new ArrayList<>(); is not allowed here
-        // var<Integer> list1 = new ArrayList<>(); not allowed
-        // var list1 = new ArrayList<>(); valid
-        // var k; not valid
-        // var k = 5;  valid
-
-        var test = new TestClass(5){
-            // static int x = 2;
-            // final static int x = 2;
-            // static int p = 10;
+    
+       enum WeekDays {
+        MONDAY("Working day") {
             @Override
             public void testMethod() {
-                System.out.println("Test 2");
+                System.out.println("MONDAY method");
             }
+        }, TUESDAY {
+            @Override
+            public void testMethod() {
+                System.out.println("TUESDAY method");
+            }
+         }; // WEDNESDAY, THURSDAY, FRIDAY("Day off"), SATURDAY;
 
-            public void testMethod2() {
-                System.out.println(d);
-            } 
-        };
-        test.testMethod2();
-        
-        
-    // var string = "String";
-    // var n = 1;
-    // var c = 'C';
-    // var b = true;
-    // var t = new TestClass();
-    
+        private String status;
+        WeekDays(String status) {
+            this.status = status;
+        }
+
+        WeekDays() {}
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public abstract void testMethod();
     };
-}   
+
+    // WeekDays.TUESDAY.testMethod();
+    System.out.println(DayOfWeek.FRIDAY);
+    System.out.println(Month.JANUARY);
+ }  
+
+}
