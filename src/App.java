db968bf7fe54ee28a770c03f52acf06e15ea39c0
Osama Salih <osamasalih.ca@gmail.com>
2025-02-02 13:53:21 +0300

    Reader class | Writer class

--- src/App.java
+++ src/App.java
@@ -1,26 +1,59 @@
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        File f = new File("abc.txt");
-        System.out.println(f.exists());
-        f.createNewFile();
+        // String s = "Hello";
 
-        if (f.exists()) {
-            System.out.println("File name: " + f.getName());
-            System.out.println("Absolute path: " + f.getAbsolutePath());
-            System.out.println("file path: " + f.getPath());
-            System.out.println("Readable: " + f.canRead());
-            System.out.println("Writeable: " + f.canWrite());
-            System.out.println("File size in bytes: " + f.length());
-            System.out.println("Is directory: " + f.isDirectory());
-            System.out.println("Is file: " + f.isFile());
-            System.out.println("Is hidden: " + f.isHidden());
-            System.out.println("Last modified: " + f.lastModified());
-            System.out.println("Can execute: " + f.canExecute());
-            System.out.println("Can read: " + f.canRead());
-            System.out.println("Can write: " + f.canWrite());
-        }
+        // Writer writer = new OutputStreamWriter(new FileOutputStream("test.txt"));
+        // writer.write(s);
+        // writer.flush();
+        // writer.close();
+
+    //  String s = "Hello";
+    //  FileWriter fr = new FileWriter("test.txt");
+    //  fr.write("AA");
+    //  fr.write("\n");
+    //  fr.write("BB");
+    //  fr.close();
+
+        
+    // FileOutputStream fos = new FileOutputStream("test.txt");
+    // String s = "Hello";
+    // byte b[] = s.getBytes();
+    // fos.write(b);
+    // fos.close();
+
+    // String s = "Hello osama";
+    // FileWriter fr = new FileWriter("test.txt");
+    // fr.write(s, 1, 2);
+    // fr.write('97);
+    // fr.write('b');
+    // fr.write("AA");
+    // fr.write("\n");
+    // fr.write("BB");
+    // fr.close();
+
+    // File file = new File("test.txt");
+    // FileReader fr = new FileReader(file);
+    // int i;
+    // while ((i = fr.read()) != -1) {
+    //     System.out.print(i);
+    // }
+    // }
+    File file = new File("test.txt");
+    FileReader fr = new FileReader(file);
+
+    char ch[] = new char[(int)file.length()];
+    fr.read(ch);
+    for (char c : ch) {
+        System.out.print(c);
     }
+    fr.close();
+}
 }
